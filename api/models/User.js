e8250ea290a8e5b6b67601f09ac6e113176ea85b
Evan <evan.straw99@gmail.com>
2018-05-05 18:50:25 -0700

    create User model

---
+++ api/models/User.js
@@ -0,0 +1,30 @@
+const Promise = require('promise');
+
+module.exports = {
+  attributes: {
+    uuid: {
+      type: 'string',
+      required: true,
+      isUUID: true,
+      unique: true
+    },
+    name: {
+      type: 'string',
+      required: true
+    },
+    email: {
+      type: 'string',
+      required: true,
+      isEmail: true,
+      unique: true
+    },
+    password:{
+      type: 'string',
+      required: true
+    },
+    admin: {
+      type: 'boolean',
+      defaultsTo: false
+    }
+  }
+};
