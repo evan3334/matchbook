4ebe586029ca088dd03229cfa40e6d1b81818ed3
Evan <evan.straw99@gmail.com>
2018-05-07 10:46:25 -0700

    remove debug prints, whoops

--- api/controllers/pages/loginpage.js
+++ api/controllers/pages/loginpage.js
@@ -24,16 +24,12 @@ module.exports = {
   fn: async function(inputs,exits){
     if(this.req.session.me){ //if user is already logged in
       let redirectPage = '/';
-      sails.log.info('yeet');
-      sails.log.info(inputs.redirect);
       if(inputs.redirect){
         redirectPage = decodeURIComponent(inputs.redirect);
       }
-      sails.log.info(redirectPage);
       return exits.redirect(redirectPage);
     }
     else{
-      sails.log.info(inputs.redirect);
       return exits.success({redirect: inputs.redirect});
     }
   }
