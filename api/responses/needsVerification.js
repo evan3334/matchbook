49b47be9987f09b978c225af6e5bd2d317bcff00
Evan <evan.straw99@gmail.com>
2018-05-19 16:53:11 -0700

    Create needsVerification response to show account verification page

---
+++ api/responses/needsVerification.js
@@ -0,0 +1,14 @@
+module.exports = function needsVerification(redirect) {
+
+  let req = this.req;
+  let res = this.res;
+  let attemptedPage;
+  if (typeof redirect === 'string') {
+    attemptedPage = redirect;
+  } else if (typeof redirect === 'boolean' && !redirect) {
+    attemptedPage = '';
+  } else {
+    attemptedPage = req.path;
+  }
+  res.redirect('/account/verify/'+((attemptedPage!=='')?'?redirect=' + encodeURIComponent(attemptedPage):''));
+};
